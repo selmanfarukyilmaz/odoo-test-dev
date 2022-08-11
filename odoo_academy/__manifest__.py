c62c5273e64462c7b0cb56c529ec08f8ef1a97cf
selmanfarukyilmaz <aksamavisi@gmail.com>
2022-08-11 14:19:14 +0000

    inital

---
+++ odoo_academy/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Odoo Academy',
+    
+    'summary': """Academy app to manage Training""",
+    
+    'description':"""
+        Academy Module to managae Training:
+        - Courses
+        - Sessions
+        - Attendees
+    """,
+    
+    'author': 'Odoo',
+    
+    'website': 'https://www.odoo.com',
+    
+    'category': 'Training',
+    'version': '1.0',
+    
+    'depends': ['base'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+
+    ],
+}
