19ac175d7c0bef3082dd95ae439ade044f5ef43c
selmanfarukyilmaz <aksamavisi@gmail.com>
2022-08-14 16:50:51 +0000

    seven done

--- odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
+++ odoo_academy/.ipynb_checkpoints/__manifest__-checkpoint.py
@@ -26,7 +26,7 @@
         'security/ir.model.access.csv',
         'views/academy_menuitems.xml',
         'views/course_views.xml',
-        'views/session_views.xml'
+        'views/session_views.xml',
     ],
     
     'demo': [
